0a2630286c929d26eba166d04f16fcb843a08d33
arkar20 <arkar20011@gmail.com>
2025-03-22 15:29:31 -0700

    map to exact col

--- main.go
+++ main.go
@@ -13,14 +13,14 @@ import (
 )
 
 type Customer struct {
-	ID             uint   `gorm:"primaryKey"`
-	Name           string `gorm:"size:255"`
-	PhNum1         string `gorm:"size:20"`
-	PhNum2         string `gorm:"size:20"`
-	Company        string `gorm:"size:255"`
-	Address        string `gorm:"size:500"`
-	RetailCustomer bool   `gorm:"default:false"`
-	MarketplaceID  uint
+	ID             uint   `gorm:"primaryKey;column:id"`
+	Name           string `gorm:"size:255;column:name"`
+	PhNum1         string `gorm:"size:20;column:phnum1"`
+	PhNum2         string `gorm:"size:20;column:phnum2"`
+	Company        string `gorm:"size:255;column:company"`
+	Address        string `gorm:"size:500;column:address"`
+	RetailCustomer bool   `gorm:"default:false;column:retail_customer"`
+	MarketplaceID  uint   `gorm:"column:marketplace_id"`
 }
 
 var db *gorm.DB
