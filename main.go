abff5e7cf7abcf756d47569f210dc779ef650aae
arkar <arkar20011@gmail.com>
2025-05-25 14:24:26 -0700

    fix

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"restapi/customer"
 	"restapi/db"
-	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -15,15 +14,7 @@ func main() {
 
 	r.Use(gin.Recovery())
 
-	r.Use(cors.New(cors.Config{
-		AllowOriginFunc: func(origin string) bool {
-			return true
-		},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
+	r.Use(cors.Default())
 
 	customer.RegisterRoutes(r, customer.NewController())
 
